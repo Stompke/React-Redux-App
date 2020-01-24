1260be95abe9e21e7db363072e18772c56cf12eb
Shawn Tompke <shawntompke@Shawns-MBP.routerlogin.net>
2020-01-24 06:44:38 -0800

    app is working good

--- recipe-maker/src/components/Form.js
+++ recipe-maker/src/components/Form.js
@@ -1,7 +1,27 @@
 import React, { useState, useEffect } from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
 import { connect } from 'react-redux'
 import { fetchData } from '../actions';
+
+
+const useStyles = makeStyles(theme => ({
+    root: {
+      '& > *': {
+        margin: theme.spacing(1),
+        width: 200,
+      },
+    },
+    button: {
+        marginBottom: '20px' 
+    },
+
+  }));
+
 
 const Form = props => {
     // const [apiCall, setApiCall] = useState('http://www.recipepuppy.com/api/')
@@ -30,17 +50,16 @@ const Form = props => {
         }
     }
 
+    const classes = useStyles();
+
 
     return (
         <div>
-<h1>{apiCall}</h1>
-            <div>
-                <span> <input onChange={mealOnChange} placeholder='meal type'/> </span>
-            </div>
-            <div>
-                <span> <input onChange={ingredientsOnChange} placeholder='ingredients'/> <button>Add Ingredients</button></span>
-            </div>
-            <button onClick={() => props.fetchData(apiCall)}>Get Meals</button>
+<form className={classes.root} noValidate autoComplete="off">
+      <TextField className={classes.input} onChange={mealOnChange}  id="standard-basic" label="Meal Type" />
+      <TextField  onChange={ingredientsOnChange} id="filled-basic" label="ingredients" variant="filled" />
+    </form>
+            <Button className={classes.button} variant="contained" color="primary" onClick={() => props.fetchData(apiCall)}>Get Meals</Button>
         </div>
     )
 }
